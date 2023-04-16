78b9c7a3311f136348da2786bb189471b68b3ea2
stevegustason <steve.gustason@gmail.com>
2023-04-16 14:03:41 -0500

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Steven Gustason on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
